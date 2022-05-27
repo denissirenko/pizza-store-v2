98d9f6f1a11c5143cc3bb039df1d2999ffca628f
Denis Sirenko <sirenko.denis86@gmail.com>
2022-05-27 22:13:13 +0300

    component pizzaBlock

---
+++ src/components/Categories.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export const Categories = () => {
+  const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+  const [activeIndex, setActiveIndex] = React.useState(0);
+
+  const onClickCategory = (index) => {
+    setActiveIndex(index);
+  };
+
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((item, index) => {
+          return (
+            <li
+              key={item}
+              onClick={() => onClickCategory(index)}
+              className={activeIndex === index ? 'active' : ''}>
+              {item}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
